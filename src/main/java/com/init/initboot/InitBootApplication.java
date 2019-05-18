a47cf96d2a2927c395c17e5d7e153f8cfd6f54ba
shaswat123 <shaswat.dasgupta@gmail.com>
2019-05-18 18:39:28 +0530

    First Commit

---
+++ src/main/java/com/init/initboot/InitBootApplication.java
@@ -0,0 +1,14 @@
+package com.init.initboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitBootApplication.class, args);
+	}
+
+}
+
